273c670b2069c0e8a75813c12aaa70b442079970
César Valentín Fernández Guevara <fern.cesar2100@gmail.com>
2025-08-18 21:50:01 -0300

    Se creo el archivo 1.2 ciclo do while de la clase 1 de JS

---
+++ JavaScript/Clase 1/1.2 Ciclo do While.js
@@ -0,0 +1,15 @@
+//while
+let contador = 0; 
+while(contador < 3){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin del ciclo while");
+
+//do While
+let conteo = 0; 
+do{ 
+    console.log(conteo); 0, 1, 2
+    conteo++;
+}while(conteo < 3);
+console.log("Fin del ciclo do while");
