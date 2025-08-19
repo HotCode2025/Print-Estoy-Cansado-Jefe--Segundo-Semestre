5d50f42c090cdd6b178afc8626d67a9c58141ecd
Abril Cisterna <abril.cisterna23@gmail.com>
2025-08-18 21:19:15 -0300

    Se agrega el video 1.2 de la Clase 2 de JS

---
+++ JavaScript/Clase 2/1.2 - Arreglos.js
@@ -0,0 +1,10 @@
+const autos = ['Ferrari', 'Renault', 'BMW'];
+console.log(autos);
+
+//Recorremos los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
+
+for (let i = 0 ; i < autos.length; i++){
+    console.log(i+' : '+autos[i]);
+}
