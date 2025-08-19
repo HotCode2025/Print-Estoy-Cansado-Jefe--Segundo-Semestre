c6de8196dbc32192f1e3ddcd6cf6f20ce829daaf
Franco Fernandez <164897425+ffrancode@users.noreply.github.com>
2025-08-18 21:45:39 -0300

    Agregamos ejercicios Clase 1 JS

--- JavaScript/Clase 1/1.1 Ciclo While.js
+++ JavaScript/Clase 1/1.1 Ciclo While.js
@@ -2,7 +2,7 @@
 
 let contador = 0;
 while(contador < 3){
-    console.log(contador);
+    console.log(contador); //el ciclo while pasa por el 0,1 y 2 y se detiene ahi.
     contador++;
 }
 console.log("Fin del ciclo while");
