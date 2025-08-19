8d1a2b982941def7c411d1dc0edf11de038908e2
Emir Sat <emirsat54@hotmail.com>
2025-08-19 12:16:05 -0300

    Ejercicio 4 de la clase 2 de Arreglos

---
+++ JavaScript/Clase 2/1.4 Preguntar si es un Array.js
@@ -0,0 +1,5 @@
+// Como preguntar si es una Array o Arreglo
+console.log(Array.isArray(autos)); //Devuelve un booleano
+
+
+console.log(autos instanceof Array); //Preguntamos si la variable es una instancia de la clase Array
