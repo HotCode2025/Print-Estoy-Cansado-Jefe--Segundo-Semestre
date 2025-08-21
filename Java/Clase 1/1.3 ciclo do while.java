ad8fccd8df86f8505a6460b346be8d8debde2a23
Roy <roybruna1580@gmail.com>
2025-08-20 22:13:24 -0300

    1.3 ciclo do while de la clase 1 Java añadido

---
+++ Java/Clase 1/1.3 ciclo do while.java
@@ -0,0 +1,17 @@
+package Ciclowhile;
+
+public class EjercicioWhile01 {
+    public static void main(String[] args) {
+        var conteo = 0; // Inferencia de tipos
+        while(conteo < 3){
+            System.out.println("conteo = " + conteo);
+            conteo++; //Vamos aumentando en uno la variable
+        }
+
+        var contador = 0;
+        do{
+            System.out.println("contador = " + contador);
+            contador++;
+        }while(contador < 7);
+    }
+}
