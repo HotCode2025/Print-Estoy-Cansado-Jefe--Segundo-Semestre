fba46e0b95b626729df0426b2519702a06cff6ff
farreceygor <129985516+farreceygor@users.noreply.github.com>
2025-08-25 20:54:59 -0300

    Agrego ejercicio 1.7

---
+++ JavaScript/Clase 3/1.7 Argumentos y parámetros.js
@@ -0,0 +1,8 @@
+//Función tipo expresión 
+let sumar = function(a = 4, b = 8){
+    console.log(arguments[0]); //muestra el parametro de: a
+    console.log(arguments[1]); //muestra el parametro de: b
+    return a + b + (arguments[2]);
+}
+resultado = sumar(3, 2, 9);
+console.log(resultado);
