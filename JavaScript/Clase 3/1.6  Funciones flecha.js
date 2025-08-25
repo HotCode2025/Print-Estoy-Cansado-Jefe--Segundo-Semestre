d13952c817dfa84f4e094e271e7142e6963b8339
Franco Fernandez <164897425+ffrancode@users.noreply.github.com>
2025-08-25 20:35:53 -0300

    Ejercicio 1.6 | Clase 3 JS 2025

---
+++ JavaScript/Clase 3/1.6  Funciones flecha.js
@@ -0,0 +1,5 @@
+//Funciones Flecha
+
+const sumarFuncionFlecha = (a,b) => a + b;
+resultado = sumarFuncionFlecha(3, 7); //Se asigna el valor a la variable definida "resultado"
+console.log(resultado);
