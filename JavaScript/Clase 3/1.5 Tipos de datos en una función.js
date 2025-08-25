e3d4d03510390c6e323ed96544cee70d82dae43f
Abril Cisterna <abril.cisterna23@gmail.com>
2025-08-25 20:29:28 -0300

    Se agrega el video 1.5 de la clase 3 de JS

---
+++ JavaScript/Clase 3/1.5 Tipos de datos en una función.js
@@ -0,0 +1,12 @@
+//console.log(typeof miFuncion);
+function miFuncionDos(a,b){
+    //console.log(arguments);
+    console.log(arguments.length);
+}
+
+//miFuncionDos(5, 7);
+miFuncionDos(5, 7, 3, 6);
+
+//toString
+var miFuncionTexto = miFuncionDos.toString();
+console.log(miFuncionTexto);
