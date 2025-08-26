9f236ff440ccb662f54fe0d39b69b3d09e93d29d
Gian Franco Albornoz <gianfranco321123@gmail.com>
2025-08-25 21:06:15 -0300

    agrego el ejercicio 1.2 mal subido anteriormente

---
+++ JavaScript/Clase 3/1.2 PalabraReturn.js
@@ -0,0 +1,10 @@
+miFuncion(8,2); //Esto se lo conoce como hosting
+function miFuncion(a,b){
+    //console.log("sumamos: "+ (a+b));
+return a+b;
+}
+
+// LLamando la funcion
+miFuncion (5,4);
+let resultado = miFuncion (6,7);
+console.log(resultado);
