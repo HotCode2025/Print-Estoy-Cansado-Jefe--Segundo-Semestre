978f3a182de4b8992d3abfc21217a87eedf967a5
Nicolas5718 <164242580+Nicolas5718@users.noreply.github.com>
2025-09-07 18:41:45 -0300

    agrego el ejercicio 4.6 de Java

---
+++ Java/Clase 4/4.6 CreacionDeMetodos.java
@@ -0,0 +1,14 @@
+
+package creacionDeMetodos;
+
+public class Aritmetica {
+    //atributos de la clase
+    int a;
+    int b;
+    //Metodo
+    public void sumarNumeros() {
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+
+}
