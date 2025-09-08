d58dfb901d5534573e1eef5472f1126c782e8599
César Valentín Fernández Guevara <fern.cesar2100@gmail.com>
2025-09-08 22:09:47 +0000

    Correccion menor

---
+++ Python/Clase 5/6.9 Funciones: Argumentos, Variables en Funciones.py
@@ -0,0 +1,28 @@
+# Paso de Argumentos (funciones)
+def mi_funcion(name, lastname):
+    print("Hola " + name + " " + lastname)
+
+mi_funcion("Lionel", "Messi")
+mi_funcion("Ángel", "Di María")
+mi_funcion("Enzo", "Fernández")
+
+# La palabra return en funciones
+# Creamos una funcion para sumar
+def sumar(a, b):
+    return a + b
+
+# resultado = sumar(5, 10)
+# print("El resultado de la suma es:", resultado)
+print(f"El resultado de la suma es: {sumar(55, 25)}")
+
+def sumar2(a = 0, b = 0): # Le damos un valor por default
+    return a + b 
+resultado = sumar2(22, 66)
+print(f"El resultado de la suma es: {resultado}")
+
+#Argumentos, variables en funciones
+def listarNombres(*nombres): # El asterisco indica que puede recibir varios argumentos
+    for nombre in nombres:
+        print(nombre)
+listarNombres("Lionel", "Ángel", "Ariel", "Rosa", "Maria")
+listarNombres("Marcos", "Emir", "Cesar", "Gian", "Abril", "Roy", "Lautaro")
