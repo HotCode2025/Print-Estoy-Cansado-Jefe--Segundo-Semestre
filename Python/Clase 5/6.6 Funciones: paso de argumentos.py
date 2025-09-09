4ba7ee80ec6442d751704f44cb7bb7af09b108fb
Nicolas5718 <164242580+Nicolas5718@users.noreply.github.com>
2025-09-08 22:49:51 -0300

    Teoria de la clase 6.6

---
+++ Python/Clase 5/6.6 Funciones: paso de argumentos.py
@@ -0,0 +1,8 @@
+# Paso de argumentos (Funciones)
+def mi_funcion2(name, lastName):
+    print("Saludos a todos lo que ver a traves del canal de youtube")
+    print(f'Nombre: {name}, Apellido: {LastName}')
+mi_funcion2('Jorge', 'Lucero') 
+mi_funcion2('Analia', 'Pedrosa')
+mi_funcion2('Nicolas', 'Veloz')
+   
