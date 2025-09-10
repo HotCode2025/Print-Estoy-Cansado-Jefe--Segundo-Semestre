2bfd40dd408d108ca2d14ab98a566a17671912d6
Roy <roybruna1580@gmail.com>
2025-09-10 20:39:04 -0300

    agregamos 5.4 metodo llamando a otro metodo de Java Clase 5

---
+++ Java/Clase 5/5.4 Un método llamando a otro método.java
@@ -0,0 +1,21 @@
+public class Aritmetica {
+    // Atributos de la clase
+    int a;
+    int b;
+
+    // Métodos
+    public void sumarNumeros() {
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+
+    public int sumarConRetorno() {
+        return a + b;
+    }
+
+    public int sumarConArgumentos(int arg1, int arg2) {
+        a = arg1;
+        b = arg2;
+        return sumarConRetorno();
+    }
+}
