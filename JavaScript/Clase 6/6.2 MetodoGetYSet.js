d91b1ed60c5c4007613b1b97f2043e06860b03ac
Gian Franco <123424146+Giann19@users.noreply.github.com>
2025-09-16 00:06:01 +0000

    subo 6.2 JS

---
+++ JavaScript/Clase 6/6.2 MetodoGetYSet.js
@@ -0,0 +1,16 @@
+class Persona{
+
+    constructor(nombre, apellido){
+        this._nombre=nombre;
+        this._apellido=apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+
+}
+
+let persona1 = new Persona('Martín', 'Perez');
+console.log(persona1._nombre); 
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2._nombre);
