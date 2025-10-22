edae9b6f72ffee16bbe7c8bbc98847acf31cacb7
Emir Sat <emirsat54@hotmail.com>
2025-10-22 19:49:22 -0300

    11.1 de la Clase 11 de java

---
+++ Java/Clase 11/11.1 Arreglos Parte 1 Creamos la carpeta Leccion9.java
@@ -0,0 +1,10 @@
+
+package test;
+
+public class TestArreglos {
+    public static void main(String[] args) { //lado derec. instanciamos un objeto de tipo object
+        int edades[] = new int [3]; //el lado izq. declaramos la variable
+        System.out.println("edades = " + edades)
+
+    }
+}
