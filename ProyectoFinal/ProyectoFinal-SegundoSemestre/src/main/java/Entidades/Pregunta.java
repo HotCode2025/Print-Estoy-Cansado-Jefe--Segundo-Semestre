011617c75481cedb0570b1163e26c5b76a4158a6
Gian Franco <gianfranco321123@gmail.com>
2025-10-22 21:57:45 -0300

    Se crea estructura del proyecto
    Se creó estructura, clase Main,Entidad (se agregaron atributos, constructores, getters y setters necesarios) y Servicios con una clase vacía para futuros avances.

---
+++ ProyectoFinal/ProyectoFinal-SegundoSemestre/src/main/java/Entidades/Pregunta.java
@@ -0,0 +1,53 @@
+package Entidades;
+
+import java.util.List;
+
+public class Pregunta {
+
+    private int id;
+    private String texto;
+    private List<String> opciones;
+    private int indiceCorrecto;
+
+    public Pregunta() {
+    }
+
+    public Pregunta(int id, String texto, List<String> opciones, int indiceCorrecto) {
+        this.id = id;
+        this.texto = texto;
+        this.opciones = opciones;
+        this.indiceCorrecto = indiceCorrecto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public List<String> getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(List<String> opciones) {
+        this.opciones = opciones;
+    }
+
+    public int getIndiceCorrecto() {
+        return indiceCorrecto;
+    }
+
+    public void setIndiceCorrecto(int indiceCorrecto) {
+        this.indiceCorrecto = indiceCorrecto;
+    }
+}
